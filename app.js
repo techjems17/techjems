08e0ab0a81f9c2b0dbf84b32f28e5f2efec01fcd
Jordan Smith <techjems17@gmail.com>
2018-05-09 11:47:21 -0600

    updated resume download

---
+++ app.js
@@ -0,0 +1,13 @@
+var modal = document.getElementsByClassName("modal")[0];
+var small = document.getElementsByClassName("small-res")[0];
+var large = document.getElementsByClassName("large-res")[0];
+var close = document.getElementsByClassName("close")[0];
+
+small.onclick = () => {
+    modal.style.display = "block";
+    large.src = this.src;
+}
+
+close.onclick = () => {
+    modal.style.display = "none";
+}
